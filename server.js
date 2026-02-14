a7aa83a58608be91ea2618155ec3c8330de26d0b
Nicolas Wickens <nicolaswickens@gmail.com>
2026-02-14 21:42:47 +0200

    password first step, not put into database yet

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
-const path = require('path');
-const fastify = require('fastify')({ logger: true });
-const fastifyStatic = require('@fastify/static');
+const path = require("path");
+const fastify = require("fastify")({ logger: true });
+const fastifyStatic = require("@fastify/static");
 
 fastify.register(fastifyStatic, {
-  root: path.join(__dirname, 'front_end'),
-  prefix: '/', 
+  root: path.join(__dirname, "front_end"),
+  prefix: "/",
 });
 
-fastify.get('/', (req, reply) => {
-  reply.sendFile('index.html');
+fastify.get("/", (req, reply) => {
+  reply.sendFile("index.html");
+});
+// password hashing
+const bcrypt = require("bcrypt");
+fastify.post("/register", async (req, reply) => {
+  console.log("Received registration data: ", req.body);
+  const { username, password, email } = req.body;  
+  const saltRounds = 10;
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
+    console.log("Hashed Password:", hash);
+    // Here you would typically save the username, hashed password, and email to your database
+    reply
+      .status(201)
+      .send({ success: true, message: "User registered successfully" });
+  } catch (err) {
+    console.error("Error hashing password:", err);
+    reply.status(500).send({ error: "Internal Server Error" });
+  }
 });
 
 const port = Number(process.env.PORT) || 3000;
-fastify.listen({ port, host: '0.0.0.0' });
+fastify.listen({ port, host: "0.0.0.0" });
